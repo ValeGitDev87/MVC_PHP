9cdc38a2da991faf625c7d1e6e19f044443a45b9
ban adv <banadv@iMac-di-Web-2.homenet.telecomitalia.it>
2024-06-19 16:37:40 +0200

    inserito autoloder

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+define('SITE_NAME', 'tutorial-site');
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', '');
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tutorial_site_db');
+
+echo APP_ROOT;
